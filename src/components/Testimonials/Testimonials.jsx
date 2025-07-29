cccfb6a3fb5180bd9f506e79d2729998299847e4
aasthathakur534 <aasthakur1947@gmail.com>
2025-07-30 01:36:27 +0530

    feat: add students reviews

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -12,10 +12,77 @@ import user_4 from '../../assets/user-4.png'
 
 const Testimonials = () => {
   return (
-    <div claasName='testimonials'>
+    <div className='testimonials'>
         <img src={next_icon} alt="" className='next-btn' />
         <img src={back_icon} alt="" className='back-btn' />
-        <div className="slider"></div>
+        <div className="slider">
+            <ul>
+                <li>
+                    <div className="slide">
+                        <div className="user-info">
+                            <img src={user_1} alt="" />
+                            <div>
+                                <h3>William Jackson</h3>
+                                <span>Edusity, USA</span>
+                            </div>  
+                        </div>
+                        <p>
+                            I have chosen to pursue my career through Edusity because of its strong commitment to quality education, practical learning, and industry-relevant training. Edusity’s well-structured curriculum, experienced faculty, and focus on real-world skills align perfectly with my career goals and aspirations. I believe this platform will provide me with the right foundation, exposure, and guidance to grow both academically and professionally in today’s competitive landscape.
+
+
+                        </p>
+                    </div>
+                </li>
+                <li>
+                    <div className="slide">
+                        <div className="user-info">
+                            <img src={user_2} alt="" />
+                            <div>
+                                <h3>William Jackson</h3>
+                                <span>Edusity, USA</span>
+                            </div>  
+                        </div>
+                        <p>
+                            I have chosen to pursue my career through Edusity because of its strong commitment to quality education, practical learning, and industry-relevant training. Edusity’s well-structured curriculum, experienced faculty, and focus on real-world skills align perfectly with my career goals and aspirations. I believe this platform will provide me with the right foundation, exposure, and guidance to grow both academically and professionally in today’s competitive landscape.
+
+
+                        </p>
+                    </div>
+                </li>
+                <li>
+                    <div className="slide">
+                        <div className="user-info">
+                            <img src={user_3} alt="" />
+                            <div>
+                                <h3>William Jackson</h3>
+                                <span>Edusity, USA</span>
+                            </div>  
+                        </div>
+                        <p>
+                            I have chosen to pursue my career through Edusity because of its strong commitment to quality education, practical learning, and industry-relevant training. Edusity’s well-structured curriculum, experienced faculty, and focus on real-world skills align perfectly with my career goals and aspirations. I believe this platform will provide me with the right foundation, exposure, and guidance to grow both academically and professionally in today’s competitive landscape.
+
+
+                        </p>
+                    </div>
+                </li>
+                <li>
+                    <div className="slide">
+                        <div className="user-info">
+                            <img src={user_4} alt="" />
+                            <div>
+                                <h3>William Jackson</h3>
+                                <span>Edusity, USA</span>
+                            </div>  
+                        </div>
+                        <p>
+                            I have chosen to pursue my career through Edusity because of its strong commitment to quality education, practical learning, and industry-relevant training. Edusity’s well-structured curriculum, experienced faculty, and focus on real-world skills align perfectly with my career goals and aspirations. I believe this platform will provide me with the right foundation, exposure, and guidance to grow both academically and professionally in today’s competitive landscape.
+
+
+                        </p>
+                    </div>
+                </li>
+            </ul>
+        </div>
 
       
 
